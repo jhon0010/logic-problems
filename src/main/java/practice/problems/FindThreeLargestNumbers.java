bcf179eb4455bb4566bd4e589a61844d7942e595
jhon0010 <jhoning.soft@gmail.com>
2020-05-29 01:48:25 -0500

    FindThreeLargestNumbers set And Shift Values

--- src/main/java/practice/problems/FindThreeLargestNumbers.java
+++ src/main/java/practice/problems/FindThreeLargestNumbers.java
@@ -12,7 +12,7 @@ public class FindThreeLargestNumbers {
 
     public static void main(String[] args) {
         var array = new int[] {141,8,-9,-7,-21,-6};
-        LOGGER.info("Result = " + Arrays.toString(findThreeLargestNumbers(array)));
+        LOGGER.info("Result = " + Arrays.toString(findThreeLargestNumbersShifthing(array)));
     }
 
     public static int[] findThreeLargestNumbers(int[] array) {
@@ -33,6 +33,35 @@ public class FindThreeLargestNumbers {
             }
         }
         return largesNumbers;
+    }
+
+
+    public static int[] findThreeLargestNumbersShifthing(int[] array) {
+
+        final int min = -Integer.MAX_VALUE;
+        var largesNumbers = new int[]{min, min, min};
+
+        for(int i : array){
+            if(largesNumbers[2] > min - 1 && i > largesNumbers[2]) {
+                setAndShiftValues(largesNumbers, i, 2);
+            } else if(largesNumbers[1] > min - 1 && i > largesNumbers[1]) {
+                setAndShiftValues(largesNumbers, i, 1);
+            }else if(largesNumbers[0] > min - 1  && i > largesNumbers[0]) {
+                setAndShiftValues(largesNumbers, i, 0);
+            }
+        }
+        return largesNumbers;
+    }
+
+    private static void setAndShiftValues(int[] largesNumbers, int value, int index){
+        for (int i = 0; i < largesNumbers.length; i++) {
+            if(index == i){
+                largesNumbers[index] = value;
+                break;
+            } else {
+                largesNumbers[i] = largesNumbers[i + 1];
+            }
+        }
     }
 
 }
