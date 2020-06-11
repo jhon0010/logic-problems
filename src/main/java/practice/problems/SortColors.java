96cd0e4266d790275f9850dc2ba6bf29ffb61d12
jhon0010 <jhoning.soft@gmail.com>
2020-06-11 16:05:32 -0500

    Sort array problem

---
+++ src/main/java/practice/problems/SortColors.java
@@ -0,0 +1,45 @@
+package practice.problems;
+
+import org.slf4j.Logger;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * Given an array with n objects colored red, white or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white and blue.
+ *
+ * Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+ *
+ * Note: You are not suppose to use the library's sort function for this problem.
+ *
+ * Example:
+ *
+ * Input: [2,0,2,1,1,0]
+ * Output: [0,0,1,1,2,2]
+ */
+public class SortColors {
+
+    private static final Logger LOGGER = getLogger(SortColors.class);
+
+    public static void main(String[] args) {
+
+        int[] nums = {2, 0, 1, 0, 2, 1};
+        sortColors(nums);
+    }
+
+    public static void sortColors(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        for (int i = 0; i <= r; i++) {
+            if (nums[i] == 0) {
+                swap(nums, i, l++);
+            } else if (nums[i] == 2) {
+                swap(nums, i--, r--);
+            }
+        }
+    }
+    public static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+}
